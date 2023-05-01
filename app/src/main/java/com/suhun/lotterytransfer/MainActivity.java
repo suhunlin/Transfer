148c8429ba7339d8ced78c167588a9ba8e847713
suhunlin <lin.suhun@msa.hinet.net>
2023-05-01 21:21:48 +0800

    Add Data Structure MyAppData to record data

--- app/src/main/java/com/suhun/lotterytransfer/MainActivity.java
+++ app/src/main/java/com/suhun/lotterytransfer/MainActivity.java
@@ -4,15 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private MyAppData myAppData;
+    Button createBtn, goPage2Btn;
+    TextView showInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         myAppData = (MyAppData) getApplication();
+    }
+    private void initView(){
+        createBtn = findViewById(R.id.create);
+        goPage2Btn = findViewById(R.id.goPage2);
+        showInfo = findViewById(R.id.info);
     }
 
     public void createLottery(View view) {
