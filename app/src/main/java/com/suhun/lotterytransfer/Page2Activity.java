586d6e9706210e147c8b8135297a4da69acfeccd
suhunlin <lin.suhun@msa.hinet.net>
2023-05-01 22:00:47 +0800

    Show lottery num from main Page

--- app/src/main/java/com/suhun/lotterytransfer/Page2Activity.java
+++ app/src/main/java/com/suhun/lotterytransfer/Page2Activity.java
@@ -4,14 +4,43 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Page2Activity extends AppCompatActivity {
+    private MyAppData myAppData;
+    private Button exitBtn;
+    private TextView[] numTextView;
+    private int[] numResourceId = {R.id.num0, R.id.num1, R.id.num2, R.id.num3, R.id.num4, R.id.num5};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page2);
+        myAppData = (MyAppData) getApplication();
+        initView();
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        showLottery();
+    }
+
+    private void initView(){
+        numTextView = new TextView[myAppData.num.length];
+        for(int i=0;i<numResourceId.length;i++){
+            numTextView[i] = findViewById(numResourceId[i]);
+        }
+        exitBtn = findViewById(R.id.exit);
+    }
+
+    private void showLottery(){
+        for(int i=0; i<myAppData.num.length; i++){
+            numTextView[i].setText(""+myAppData.num[i]);
+        }
+    }
+
 
     public void exit(View view) {
         finish();
