da908dfc26cca1f5da3b61c14d3b888860307659
suhunlin <lin.suhun@msa.hinet.net>
2023-05-01 21:50:11 +0800

    Add layout to Page2

--- app/src/main/java/com/suhun/lotterytransfer/Page2Activity.java
+++ app/src/main/java/com/suhun/lotterytransfer/Page2Activity.java
@@ -3,6 +3,7 @@ package com.suhun.lotterytransfer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class Page2Activity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class Page2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page2);
+    }
+
+    public void exit(View view) {
+        finish();
     }
 }
