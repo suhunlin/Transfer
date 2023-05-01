10cc4c5618b7bd7b41c2cd69f956383c429d7065
suhunlin <lin.suhun@msa.hinet.net>
2023-05-01 19:58:48 +0800

    Add Main Activity layout

--- app/src/main/java/com/suhun/lotterytransfer/MainActivity.java
+++ app/src/main/java/com/suhun/lotterytransfer/MainActivity.java
@@ -3,6 +3,7 @@ package com.suhun.lotterytransfer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void createLottery(View view) {
+    }
+
+    public void goPage2(View view) {
     }
 }
